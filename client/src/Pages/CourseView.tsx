0dc22cfa3c006ff4db495ce43d7058ccef55cad1
AmineNaboulsi <naboulsiiamine@gmail.com>
2025-03-08 13:22:22 +0000

    release 1

--- client/src/Pages/CourseView.tsx
+++ client/src/Pages/CourseView.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { CalendarIcon, UserIcon, TagIcon, ClockIcon } from 'lucide-react';
+import { CalendarIcon, TagIcon, ClockIcon } from 'lucide-react';
 
-// Définir le type des props
 type CourseType = {
   cat_id: number,
   content: string,
@@ -26,7 +25,9 @@ const CourseView: React.FC<CourseViewProps> = ({ Course }) => {
   if (!Course) {
     return (
       <div className="container mx-auto px-4 py-16 flex justify-center items-center">
-        <div className="animate-pulse text-xl text-gray-500">Chargement du cours...</div>
+          <div className="col-span-4 py-20">
+                <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" className="animate-spin text-center justify-self-center will-change-transform" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><line x1="12" y1="2" x2="12" y2="6"></line><line x1="12" y1="18" x2="12" y2="22"></line><line x1="4.93" y1="4.93" x2="7.76" y2="7.76"></line><line x1="16.24" y1="16.24" x2="19.07" y2="19.07"></line><line x1="2" y1="12" x2="6" y2="12"></line><line x1="18" y1="12" x2="22" y2="12"></line><line x1="4.93" y1="19.07" x2="7.76" y2="16.24"></line><line x1="16.24" y1="7.76" x2="19.07" y2="4.93"></line></svg>
+          </div>
       </div>
     );
   }
